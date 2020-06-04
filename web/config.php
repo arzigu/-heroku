df2a43f6a4fe3833b55149b9adee20339d9e14c6
arzigu <32092668+arzigu@users.noreply.github.com>
2020-06-04 17:17:31 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
+define('APIKEY', 'AIzaSyC4c8y-lCamnGff0wH30a-BmKhGSrf2l38');//这里改成您的api key
 define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('SITE_NAME', 'Software');//网站名字
+define('TITLENAME', 'Testing');//网站名字
+define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4J');//一个随机字符串，加解密KEY
+define('EMAIL', 'cmmchk@xdd.icu');//邮箱
 ?>
